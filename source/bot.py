d184c3070d796c5c893c0201cd742e96a9f3519f
ANRION <anrion@mail.ru>
2019-08-21 23:23:45 +0300

    Update bot.py

--- source/bot.py
+++ source/bot.py
@@ -13,6 +13,10 @@ def initial_bot(use_logging=True, level_name='DEBUG'):
     bot = telebot.TeleBot(TOKEN)
     #bot = Updater(TOKEN, request_kwargs=REQUEST_KWARGS)
     logger = log('bot', 'bot.log', 'INFO')
+    keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+    keyboard1.row('Готово', 'Не Готово')
+    
+    
 
     @bot.message_handler(commands=['start'])
     def start_handler(message: telebot.types.Message):
@@ -27,17 +31,9 @@ def initial_bot(use_logging=True, level_name='DEBUG'):
     @bot.message_handler(content_types=['text'])
     def text_handler(message: telebot.types.Message):
        
-        #keyboard = telebot.types.InlineKeyboardMarkup()
-        
-        #keyboard.add(telebot.types.InlineKeyboardButton('Опубликовать', callback_data='approve'),
-        #             telebot.types.InlineKeyboardButton('Отклонить', callback_data='refuse'))
+        bot.send_message(message.chat.id, 'Привет, ты написал мне /start',reply_markup=keyboard1)
        
         
-        markup = types.ReplyKeyboardMarkup()
-        itembtnTrue = types.KeyboardButton('Готово')
-        itembtnFalse = types.KeyboardButton('Не готово')
-        markup = types.ReplyKeyboardRemove(selective=False)
-        bot.send_message(chat_id, message, reply_markup=markup)
           
         logger.info(f"It's text handler. Message from {message.from_user.id}")
 
